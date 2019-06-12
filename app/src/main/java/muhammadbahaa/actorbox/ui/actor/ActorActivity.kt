ace81ccb7f7036316bba523caf44499d65e0fab1
Muhammad Bahaa <abanoub.elkess.samaan@softxpert.com>
2019-06-12 23:15:29 +0200

    init push

---
+++ app/src/main/java/muhammadbahaa/actorbox/ui/actor/ActorActivity.kt
@@ -0,0 +1,13 @@
+package muhammadbahaa.actorbox.ui.actor
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import muhammadbahaa.actorbox.R
+
+class ActorActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
